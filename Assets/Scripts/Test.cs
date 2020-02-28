21219c52f30cac3cdedab152b0db26d05edcc7b6
SeattleSkillsCenter <cldougherty@seattleschools.org>
2020-02-28 14:30:28 -0800

    Revert "Updated Test script."
    This reverts commit 9356b78875fa996fc00b730a9c762cbdc83df752.

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Test message");
+        
     }
 
     // Update is called once per frame
